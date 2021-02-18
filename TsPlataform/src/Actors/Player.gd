943ef88dd8b2bc7ac29f4a48391c0bebe691dd5a
john01t <th3gentl3lm3nt@gmail.com>
2021-02-17 22:49:03 -0300

    update17.2

--- TsPlataform/src/Actors/Player.gd
+++ TsPlataform/src/Actors/Player.gd
@@ -1,6 +1,18 @@
 extends Actor
 
 
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+
+
+
+func _on_EnemyDetector_body_entered(body: Node) -> void:
+	queue_free()
+
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
@@ -29,3 +41,12 @@ func calculate_move_velocity(
 	if is_jump_interrupted:
 		out.y = 0.0
 	return out
+
+
+
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
+
